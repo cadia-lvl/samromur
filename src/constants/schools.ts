3b329c40a07486faa923ca0208139d1ec93ca443
staffanru <staffanh@ru.is>
2021-01-04 11:32:49 +0000

    temporary school constant

---
+++ src/constants/schools.ts
@@ -0,0 +1,864 @@
+import { School } from '../types/user';
+
+export const schools: Array<School> = [
+    {
+        name: 'Akurskóli',
+        code: 'akurs23',
+        enrollment: 546,
+    },
+    {
+        name: 'Álfhólsskóli',
+        code: 'alfho8',
+        enrollment: 658,
+    },
+    {
+        name: 'Álftanesskóli',
+        code: 'alfta53',
+        enrollment: 398,
+    },
+    {
+        name: 'Alþjóðaskólinn á Íslandi',
+        code: 'islan118',
+        enrollment: 92,
+    },
+    {
+        name: 'Árbæjarskóli',
+        code: 'arbaej11',
+        enrollment: 626,
+    },
+    {
+        name: 'Arnarskóli',
+        code: 'arnar165',
+        enrollment: 8,
+    },
+    {
+        name: 'Árskógarskóli',
+        code: 'arsko159',
+        enrollment: 21,
+    },
+    {
+        name: 'Árskóli',
+        code: 'arsko67',
+        enrollment: 347,
+    },
+    {
+        name: 'Ártúnsskóli',
+        code: 'artun93',
+        enrollment: 185,
+    },
+    {
+        name: 'Áslandsskóli',
+        code: 'aslan27',
+        enrollment: 519,
+    },
+    {
+        name: 'Auðarskóli',
+        code: 'audar120',
+        enrollment: 90,
+    },
+    {
+        name: 'Austurbæjarskóli',
+        code: 'austu45',
+        enrollment: 415,
+    },
+    {
+        name: 'Barnaskóli Hjallastefnunnar á Vífilsstöðum',
+        code: 'vifil133',
+        enrollment: 59,
+    },
+    {
+        name: 'Barnaskóli Hjallastefnunnar Reykjavík',
+        code: 'reykj117',
+        enrollment: 93,
+    },
+    {
+        name: 'Barnaskóli Hjallastefnunnar við Hjallabraut',
+        code: 'hjall129',
+        enrollment: 71,
+    },
+    {
+        name: 'Barnaskólinn á Eyrarbakka og Stokkseyri',
+        code: 'stokk104',
+        enrollment: 128,
+    },
+    {
+        name: 'Bíldudalsskóli',
+        code: 'bildu151',
+        enrollment: 35,
+    },
+    {
+        name: 'Bláskógaskóli - Laugarvatni',
+        code: 'lauga139',
+        enrollment: 51,
+    },
+    {
+        name: 'Bláskógaskóli - Reykholti',
+        code: 'reykh121',
+        enrollment: 84,
+    },
+    {
+        name: 'Blönduskóli',
+        code: 'blönd103',
+        enrollment: 136,
+    },
+    {
+        name: 'Borgarhólsskóli',
+        code: 'borga77',
+        enrollment: 274,
+    },
+    {
+        name: 'Breiðagerðisskóli',
+        code: 'breid52',
+        enrollment: 398,
+    },
+    {
+        name: 'Breiðdals-og Stöðvarfjarðarskóli',
+        code: 'stödv155',
+        enrollment: 26,
+    },
+    {
+        name: 'Breiðholtsskóli',
+        code: 'breid51',
+        enrollment: 401,
+    },
+    {
+        name: 'Brekkubæjarskóli',
+        code: 'brekk41',
+        enrollment: 455,
+    },
+    {
+        name: 'Brekkuskóli',
+        code: 'brekk32',
+        enrollment: 500,
+    },
+    {
+        name: 'Brúarásskóli',
+        code: 'brúar145',
+        enrollment: 41,
+    },
+    {
+        name: 'Brúarskóli',
+        code: 'brúar154',
+        enrollment: 28,
+    },
+    {
+        name: 'Dalskóli',
+        code: 'dalsk75',
+        enrollment: 285,
+    },
+    {
+        name: 'Dalvíkurskóli',
+        code: 'dalvi85',
+        enrollment: 218,
+    },
+    {
+        name: 'Egilsstaðaskóli',
+        code: 'egils65',
+        enrollment: 356,
+    },
+    {
+        name: 'Fellaskóli (Fellabæ)',
+        code: '(fell111',
+        enrollment: 99,
+    },
+    {
+        name: 'Fellaskóli (Reykjavík)',
+        code: '(reyk70',
+        enrollment: 325,
+    },
+    {
+        name: 'Flataskóli',
+        code: 'flata39',
+        enrollment: 477,
+    },
+    {
+        name: 'Flóaskóli',
+        code: 'flóas109',
+        enrollment: 106,
+    },
+    {
+        name: 'Flúðaskóli',
+        code: 'flúda113',
+        enrollment: 97,
+    },
+    {
+        name: 'Foldaskóli',
+        code: 'folda31',
+        enrollment: 507,
+    },
+    {
+        name: 'Fossvogsskóli',
+        code: 'fossv64',
+        enrollment: 357,
+    },
+    {
+        name: 'Framsýn - menntun',
+        code: 'mennt134',
+        enrollment: 59,
+    },
+    {
+        name: 'Garðaskóli',
+        code: 'garda24',
+        enrollment: 529,
+    },
+    {
+        name: 'Gerðaskóli',
+        code: 'gerda84',
+        enrollment: 220,
+    },
+    {
+        name: 'Giljaskóli',
+        code: 'gilja55',
+        enrollment: 394,
+    },
+    {
+        name: 'Glerárskóli',
+        code: 'glera71',
+        enrollment: 325,
+    },
+    {
+        name: 'Grandaskóli',
+        code: 'grand68',
+        enrollment: 345,
+    },
+    {
+        name: 'Grenivíkurskóli',
+        code: 'greni138',
+        enrollment: 52,
+    },
+    {
+        name: 'Grímseyjarskóli',
+        code: 'grims169',
+        enrollment: 3,
+    },
+    {
+        name: 'Grundaskóli',
+        code: 'grund10',
+        enrollment: 638,
+    },
+    {
+        name: 'Grunnskóli Bolungarvíkur',
+        code: 'bolun102',
+        enrollment: 141,
+    },
+    {
+        name: 'Grunnskóli Borgarfjarðar',
+        code: 'borga92',
+        enrollment: 186,
+    },
+    {
+        name: 'Grunnskóli Borgarfjarðar eystri',
+        code: 'eystr167',
+        enrollment: 5,
+    },
+    {
+        name: 'Grunnskóli Djúpavogs/Djúpavogsskóli',
+        code: 'djúpa126',
+        enrollment: 75,
+    },
+    {
+        name: 'Grunnskóli Fáskrúðsfjarðar',
+        code: 'faskr116',
+        enrollment: 94,
+    },
+    {
+        name: 'Grunnskóli Fjallabyggðar',
+        code: 'fjall89',
+        enrollment: 204,
+    },
+    {
+        name: 'Grunnskóli Grindavíkur',
+        code: 'grind30',
+        enrollment: 509,
+    },
+    {
+        name: 'Grunnskóli Grundarfjarðar',
+        code: 'grund112',
+        enrollment: 97,
+    },
+    {
+        name: 'Grunnskóli Hornafjarðar',
+        code: 'horna80',
+        enrollment: 238,
+    },
+    {
+        name: 'Grunnskóli Húnaþings vestra',
+        code: 'vestr98',
+        enrollment: 151,
+    },
+    {
+        name: 'Grunnskóli Önundarfjarðar',
+        code: 'önund160',
+        enrollment: 18,
+    },
+    {
+        name: 'Grunnskóli Reyðarfjarðar',
+        code: 'reyda86',
+        enrollment: 217,
+    },
+    {
+        name: 'Grunnskóli Seltjarnarness',
+        code: 'seltj21',
+        enrollment: 551,
+    },
+    {
+        name: 'Grunnskóli Snæfellsbæjar',
+        code: 'snaefe82',
+        enrollment: 231,
+    },
+    {
+        name: 'Grunnskóli Vestmannaeyja',
+        code: 'vestm28',
+        enrollment: 513,
+    },
+    {
+        name: 'Grunnskólinn á Drangsnesi',
+        code: 'drang166',
+        enrollment: 8,
+    },
+    {
+        name: 'Grunnskólinn á Eskifirði',
+        code: 'eskif99',
+        enrollment: 150,
+    },
+    {
+        name: 'Grunnskólinn á Hellu',
+        code: 'hellu107',
+        enrollment: 119,
+    },
+    {
+        name: 'Grunnskólinn á Hólmavík',
+        code: 'hólma141',
+        enrollment: 44,
+    },
+    {
+        name: 'Grunnskólinn á Ísafirði',
+        code: 'isafi60',
+        enrollment: 368,
+    },
+    {
+        name: 'Grunnskólinn á Raufarhöfn',
+        code: 'raufa164',
+        enrollment: 11,
+    },
+    {
+        name: 'Grunnskólinn á Tálknafirði',
+        code: 'talkn146',
+        enrollment: 40,
+    },
+    {
+        name: 'Grunnskólinn á Þórshöfn',
+        code: 'thórsh125',
+        enrollment: 76,
+    },
+    {
+        name: 'Grunnskólinn austan Vatna',
+        code: 'vatna132',
+        enrollment: 65,
+    },
+    {
+        name: 'Grunnskólinn í Borgarnesi',
+        code: 'borga73',
+        enrollment: 297,
+    },
+    {
+        name: 'Grunnskólinn í Hofgarði',
+        code: 'hofga168',
+        enrollment: 5,
+    },
+    {
+        name: 'Grunnskólinn í Hveragerði',
+        code: 'hvera59',
+        enrollment: 370,
+    },
+    {
+        name: 'Grunnskólinn í Sandgerði',
+        code: 'sandg78',
+        enrollment: 256,
+    },
+    {
+        name: 'Grunnskólinn í Stykkishólmi',
+        code: 'stykk97',
+        enrollment: 155,
+    },
+    {
+        name: 'Grunnskólinn í Þorlákshöfn',
+        code: 'thorla83',
+        enrollment: 221,
+    },
+    {
+        name: 'Grunnskólinn Suðureyri',
+        code: 'sudur144',
+        enrollment: 41,
+    },
+    {
+        name: 'Grunnskólinn Þingeyri',
+        code: 'thinge153',
+        enrollment: 31,
+    },
+    {
+        name: 'Háaleitisskóli',
+        code: 'haale25',
+        enrollment: 521,
+    },
+    {
+        name: 'Háaleitisskóli (Reykjanesbæ)',
+        code: '(reyk74',
+        enrollment: 290,
+    },
+    {
+        name: 'Hagaskóli',
+        code: 'hagas19',
+        enrollment: 575,
+    },
+    {
+        name: 'Hamraskóli',
+        code: 'hamra94',
+        enrollment: 170,
+    },
+    {
+        name: 'Háteigsskóli',
+        code: 'hatei42',
+        enrollment: 451,
+    },
+    {
+        name: 'Helgafellsskóli',
+        code: 'olliiii',
+        enrollment: 153,
+    },
+    {
+        name: 'Heiðarskóli (Leirársveit)',
+        code: '(leir115',
+        enrollment: 94,
+    },
+    {
+        name: 'Heiðarskóli (Reykjanesbæ)',
+        code: '(reyk49',
+        enrollment: 406,
+    },
+    {
+        name: 'Hlíðarhúsaskóli',
+        code: 'hlida170',
+        enrollment: 14,
+    },
+    {
+        name: 'Hlíðarskóli',
+        code: 'hlida161',
+        enrollment: 16,
+    },
+    {
+        name: 'Hlíðaskóli',
+        code: 'hlida37',
+        enrollment: 485,
+    },
+    {
+        name: 'Höfðaskóli',
+        code: 'höfda123',
+        enrollment: 78,
+    },
+    {
+        name: 'Hofsstaðaskóli',
+        code: 'hofss18',
+        enrollment: 577,
+    },
+    {
+        name: 'Hólabrekkuskóli',
+        code: 'hólab26',
+        enrollment: 520,
+    },
+    {
+        name: 'Holtaskóli',
+        code: 'holta46',
+        enrollment: 413,
+    },
+    {
+        name: 'Hörðuvallaskóli',
+        code: 'hördu2',
+        enrollment: 922,
+    },
+    {
+        name: 'Hrafnagilsskóli',
+        code: 'hrafn101',
+        enrollment: 144,
+    },
+    {
+        name: 'Hraunvallaskóli',
+        code: 'hraun3',
+        enrollment: 798,
+    },
+    {
+        name: 'Hríseyjarskóli',
+        code: 'hrise163',
+        enrollment: 13,
+    },
+    {
+        name: 'Húnavallaskóli',
+        code: 'húnav149',
+        enrollment: 37,
+    },
+    {
+        name: 'Húsaskóli',
+        code: 'húsas96',
+        enrollment: 165,
+    },
+    {
+        name: 'Hvaleyrarskóli',
+        code: 'hvale48',
+        enrollment: 410,
+    },
+    {
+        name: 'Hvolsskóli',
+        code: 'hvols81',
+        enrollment: 238,
+    },
+    {
+        name: 'Ingunnarskóli',
+        code: 'ingun56',
+        enrollment: 391,
+    },
+    {
+        name: 'Kársnesskóli',
+        code: 'karsn17',
+        enrollment: 583,
+    },
+    {
+        name: 'Kelduskóli',
+        code: 'keldu63',
+        enrollment: 358,
+    },
+    {
+        name: 'Kerhólsskóli',
+        code: 'kerhó150',
+        enrollment: 36,
+    },
+    {
+        name: 'Kirkjubæjarskóli',
+        code: 'kirkj147',
+        enrollment: 40,
+    },
+    {
+        name: 'Klébergsskóli',
+        code: 'klebe105',
+        enrollment: 125,
+    },
+    {
+        name: 'Klettaskóli',
+        code: 'klett106',
+        enrollment: 119,
+    },
+    {
+        name: 'Kópavogsskóli',
+        code: 'kópav62',
+        enrollment: 360,
+    },
+    {
+        name: 'Krikaskóli',
+        code: 'krika108',
+        enrollment: 109,
+    },
+    {
+        name: 'Lækjarskóli',
+        code: 'laekja33',
+        enrollment: 498,
+    },
+    {
+        name: 'Lágafellsskóli',
+        code: 'lagaf6',
+        enrollment: 685,
+    },
+    {
+        name: 'Landakotsskóli',
+        code: 'landa79',
+        enrollment: 249,
+    },
+    {
+        name: 'Langholtsskóli',
+        code: 'langh7',
+        enrollment: 682,
+    },
+    {
+        name: 'Laugalækjarskóli',
+        code: 'lauga69',
+        enrollment: 326,
+    },
+    {
+        name: 'Laugalandsskóli í Holtum',
+        code: 'holtu127',
+        enrollment: 75,
+    },
+    {
+        name: 'Laugargerðisskóli',
+        code: 'lauga162',
+        enrollment: 14,
+    },
+    {
+        name: 'Laugarnesskóli',
+        code: 'lauga22',
+        enrollment: 547,
+    },
+    {
+        name: 'Lindaskóli',
+        code: 'linda36',
+        enrollment: 488,
+    },
+    {
+        name: 'Lundarskóli',
+        code: 'lunda34',
+        enrollment: 491,
+    },
+    {
+        name: 'Melaskóli',
+        code: 'melas15',
+        enrollment: 592,
+    },
+    {
+        name: 'Myllubakkaskóli',
+        code: 'myllu61',
+        enrollment: 362,
+    },
+    {
+        name: 'Naustaskóli',
+        code: 'naust54',
+        enrollment: 397,
+    },
+    {
+        name: 'Nesskóli',
+        code: 'nessk87',
+        enrollment: 217,
+    },
+    {
+        name: 'Njarðvíkurskóli',
+        code: 'njard50',
+        enrollment: 403,
+    },
+    {
+        name: 'Norðlingaskóli',
+        code: 'nordl13',
+        enrollment: 607,
+    },
+    {
+        name: 'Oddeyrarskóli',
+        code: 'oddey91',
+        enrollment: 191,
+    },
+    {
+        name: 'Ölduselsskóli',
+        code: 'öldus38',
+        enrollment: 482,
+    },
+    {
+        name: 'Öldutúnsskóli',
+        code: 'öldut20',
+        enrollment: 568,
+    },
+    {
+        name: 'Öxarfjarðarskóli',
+        code: 'öxarf157',
+        enrollment: 24,
+    },
+    {
+        name: 'Patreksskóli',
+        code: 'vestu114',
+        enrollment: 95,
+    },
+    {
+        name: 'Réttarholtsskóli',
+        code: 'retta57',
+        enrollment: 391,
+    },
+    {
+        name: 'Reykhólaskóli',
+        code: 'reykh143',
+        enrollment: 42,
+    },
+    {
+        name: 'Reykjahlíðarskóli',
+        code: 'reykj152',
+        enrollment: 35,
+    },
+    {
+        name: 'Rimaskóli',
+        code: 'rimas29',
+        enrollment: 511,
+    },
+    {
+        name: 'Sæmundarskóli',
+        code: 'saemun35',
+        enrollment: 489,
+    },
+    {
+        name: 'Salaskóli',
+        code: 'salas16',
+        enrollment: 587,
+    },
+    {
+        name: 'Selásskóli',
+        code: 'selas88',
+        enrollment: 206,
+    },
+    {
+        name: 'Seljaskóli',
+        code: 'selja9',
+        enrollment: 642,
+    },
+    {
+        name: 'Setbergsskóli',
+        code: 'setbe44',
+        enrollment: 420,
+    },
+    {
+        name: 'Seyðisfjarðarskóli',
+        code: 'seydi130',
+        enrollment: 71,
+    },
+    {
+        name: 'Síðuskóli',
+        code: 'sidus58',
+        enrollment: 372,
+    },
+    {
+        name: 'Sjálandsskóli',
+        code: 'sjala76',
+        enrollment: 275,
+    },
+    {
+        name: 'Skarðshlíðarskóli',
+        code: 'skard100',
+        enrollment: 144,
+    },
+    {
+        name: 'Skóli Ísaks Jónssonar',
+        code: 'jónss90',
+        enrollment: 196,
+    },
+    {
+        name: 'Smáraskóli',
+        code: 'smara47',
+        enrollment: 412,
+    },
+    {
+        name: 'Snælandsskóli',
+        code: 'snaela43',
+        enrollment: 432,
+    },
+    {
+        name: 'Stapaskóli',
+        code: 'sunneva',
+        enrollment: 137,
+    },
+    {
+        name: 'Stóru-Vogaskóli',
+        code: 'stóru95',
+        enrollment: 168,
+    },
+    {
+        name: 'Stórutjarnaskóli',
+        code: 'stóru148',
+        enrollment: 39,
+    },
+    {
+        name: 'Súðavíkurskóli',
+        code: 'súdav156',
+        enrollment: 24,
+    },
+    {
+        name: 'Suðurhlíðarskóli',
+        code: 'sudur142',
+        enrollment: 43,
+    },
+    {
+        name: 'Sunnulækjarskóli',
+        code: 'sunnu4',
+        enrollment: 711,
+    },
+    {
+        name: 'Tjarnarskóli',
+        code: 'tjarn137',
+        enrollment: 52,
+    },
+    {
+        name: 'Urriðaholtsskóli',
+        code: 'urrid158',
+        enrollment: 21,
+    },
+    {
+        name: 'Vættaskóli',
+        code: 'vaetta40',
+        enrollment: 465,
+    },
+    {
+        name: 'Vallaskóli',
+        code: 'valla14',
+        enrollment: 607,
+    },
+    {
+        name: 'Valsárskóli',
+        code: 'valsa135',
+        enrollment: 55,
+    },
+    {
+        name: 'Varmahlíðarskóli',
+        code: 'varma110',
+        enrollment: 104,
+    },
+    {
+        name: 'Varmárskóli',
+        code: 'varma1',
+        enrollment: 931,
+    },
+    {
+        name: 'Vatnsendaskóli',
+        code: 'vatns12',
+        enrollment: 610,
+    },
+    {
+        name: 'Vesturbæjarskóli',
+        code: 'vestu66',
+        enrollment: 351,
+    },
+    {
+        name: 'Víðistaðaskóli',
+        code: 'vidis5',
+        enrollment: 702,
+    },
+    {
+        name: 'Víkurskóli',
+        code: 'mýrda136',
+        enrollment: 53,
+    },
+    {
+        name: 'Vogaskóli',
+        code: 'vogas72',
+        enrollment: 309,
+    },
+    {
+        name: 'Vopnafjarðarskóli',
+        code: 'vopna122',
+        enrollment: 79,
+    },
+    {
+        name: 'Waldorfskólinn Lækjarbotnum',
+        code: 'laekja124',
+        enrollment: 77,
+    },
+    {
+        name: 'Waldorfskólinn Sólstafir',
+        code: 'sólst119',
+        enrollment: 91,
+    },
+    {
+        name: 'Þelamerkurskóli',
+        code: 'thelam128',
+        enrollment: 73,
+    },
+    {
+        name: 'Þingeyjarskóli',
+        code: 'thinge131',
+        enrollment: 67,
+    },
+    {
+        name: 'Þjórsárskóli',
+        code: 'thjórs140',
+        enrollment: 46,
+    },
+];
