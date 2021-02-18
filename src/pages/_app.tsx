4d85fae56851c293813881839a64931a63ef888e
staffanru <staffanh@ru.is>
2021-02-18 10:23:27 +0000

    move meta info from document to app
    compiler complained

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -149,6 +149,90 @@ class MyApp extends App<Props> {
                         <GlobalStyle />
                         <Head>
                             <title>Samrómur</title>
+                            <meta
+                                name="msapplication-TileColor"
+                                content="#da532c"
+                            />
+                            <meta name="theme-color" content="#ffffff" />
+
+                            <meta name="application-name" content="Samromur" />
+                            <meta
+                                name="apple-mobile-web-app-capable"
+                                content="yes"
+                            />
+                            <meta
+                                name="apple-mobile-web-app-status-bar-style"
+                                content="default"
+                            />
+                            <meta
+                                name="apple-mobile-web-app-title"
+                                content="Samromur"
+                            />
+                            <meta
+                                name="description"
+                                content="Raddgagnasofnun"
+                            />
+                            <meta
+                                name="format-detection"
+                                content="telephone=no"
+                            />
+                            <meta name="mobile-web-app-capable" content="yes" />
+                            <meta
+                                name="msapplication-config"
+                                content="/favicon/browserconfig.xml"
+                            />
+                            <meta
+                                name="msapplication-TileColor"
+                                content="#2B5797"
+                            />
+                            <meta
+                                name="msapplication-tap-highlight"
+                                content="no"
+                            />
+                            <meta name="theme-color" content="#000000" />
+                            <meta
+                                name="viewport"
+                                content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+                            />
+
+                            <link
+                                rel="shortcut icon"
+                                href="/favicon/favicon.ico"
+                            />
+
+                            <meta name="twitter:card" content="summary" />
+                            <meta
+                                name="twitter:url"
+                                content="https://samromur.is"
+                            />
+                            <meta name="twitter:title" content="Samromur" />
+                            <meta
+                                name="twitter:description"
+                                content="Raddgagnasofnun"
+                            />
+                            <meta
+                                name="twitter:image"
+                                content="https://samromur.is/favicon/android-chrome-192x192.png"
+                            />
+                            <meta
+                                name="twitter:creator"
+                                content="@OlafurHelgi"
+                            />
+                            <meta property="og:type" content="website" />
+                            <meta property="og:title" content="Samromur" />
+                            <meta
+                                property="og:description"
+                                content="Raddgagnasofnun"
+                            />
+                            <meta property="og:site_name" content="Samromur" />
+                            <meta
+                                property="og:url"
+                                content="https://samromur.is"
+                            />
+                            <meta
+                                property="og:image"
+                                content="https://samromur.is/favicon/apple-touch-icon.png"
+                            />
                         </Head>
                         <Component {...pageProps} />
                     </ThemeProvider>
