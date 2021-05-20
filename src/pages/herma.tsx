b5a64bb41975882c1b3ba74dabf97ecf0afe8efc
staffanru <staffanh@ru.is>
2021-05-20 17:31:59 +0000

    weekly stats handle herma

--- src/pages/herma.tsx
+++ src/pages/herma.tsx
@@ -5,6 +5,7 @@ import { RootState } from 'typesafe-actions';
 import Contribute from '../components/contribute/setup/contribute';
 import { fetchClipsToRepeat } from '../services/contribute-api';
 import { resetContribute } from '../store/contribute/actions';
+import { fetchWeeklyClips } from '../store/stats/actions';
 import { ContributeType } from '../types/contribute';
 import { Clip } from '../types/samples';
 import makeSSRDispatch from '../utilities/ssr-request';
@@ -21,14 +22,14 @@ interface Props {
 
 class RepeatPage extends React.Component<Props> {
     static async getInitialProps(ctx: NextPageContext) {
-        const { isServer, req, res, store } = ctx;
+        const { isServer, req, store } = ctx;
 
         // Reset session progress
         store.dispatch(resetContribute());
 
         // // Fetch some stats to display at the end of the session
         //TODO: make this work for SSR dispatch
-        // makeSSRDispatch(ctx, fetchWeeklyClips.request);
+        makeSSRDispatch(ctx, fetchWeeklyClips.request);
 
         // Fetch clips to prompt the user with
         const host = isServer && req ? 'http://' + req.headers.host : undefined;
