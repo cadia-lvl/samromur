525813f562a65e4aa7dfa4c333f8f9db1868888a
staffan <staffanh@ru.is>
2021-10-29 16:20:35 +0000

    fix warnings in robot icons

--- src/components/ui/icons/robot-blue.tsx
+++ src/components/ui/icons/robot-blue.tsx
@@ -38,9 +38,9 @@ export const Robot: React.FC<IconProps> = (props) => (
         <g
             id="Homepage"
             stroke="none"
-            stroke-width="1"
+            strokeWidth="1"
             fill="none"
-            fill-rule="evenodd"
+            fillRule="evenodd"
             transform="matrix(1, 0, 0, 1, 68.573402, 13.742787)"
         >
             <g
@@ -52,11 +52,7 @@ export const Robot: React.FC<IconProps> = (props) => (
                         id="robo/1.eq-n"
                         transform="translate(345.000000, 14.000000)"
                     >
-                        <g
-                            id="rooo_copy_3"
-                            stroke-width="1"
-                            fill-rule="evenodd"
-                        >
+                        <g id="rooo_copy_3" strokeWidth="1" fillRule="evenodd">
                             <g
                                 id="Shape"
                                 transform="translate(0.000000, 205.600000)"
@@ -64,7 +60,7 @@ export const Robot: React.FC<IconProps> = (props) => (
                             <path
                                 d="M146.036272,7.61411847 L146.336159,7.22089142 L145.990427,7.20967535 L145.715199,7.21680393 L146.036272,7.61411847 Z M146.036272,7.61411847 L146.456112,8.1336546 C146.235303,8.31209158 145.830854,8.31212832 145.624161,8.15449804 L146.036272,7.61411847 Z M144.947635,6.26697387 C145.598105,5.74132497 146.417518,5.74125055 147.079542,6.24613042 L147.536568,6.59467239 L148.573008,46.6107211 L143.222708,46.6107211 L144.520319,6.61229108 L144.947635,6.26697387 Z M146.000289,-1.12619346 C148.293021,-1.12559193 150.151273,0.733309705 150.151072,3.02604192 C150.150872,5.31877414 148.292295,7.17735072 145.999563,7.17755123 C143.706831,7.17775174 141.847929,5.31950027 141.847328,3.02676812 C141.847039,1.92524566 142.284489,0.86875571 143.063383,0.0898616793 C143.842277,-0.689032351 144.898767,-1.12648246 146.000289,-1.12619346 Z"
                                 id="Shape"
-                                stroke-width="2.4"
+                                strokeWidth="2.4"
                                 stroke={colors.blue2}
                             />
                             <g
@@ -75,7 +71,7 @@ export const Robot: React.FC<IconProps> = (props) => (
                                     d="M163.0936,92.7304 C168.06,92.6424 174.632,96.7008081 174.632,59.536 C174.632,22.3711919 168.06,26.4296 163.0936,26.3416 M12.2528,92.7304 C7.2864,92.6424 0.0552,92.7304 0.0552,59.536 C0.0552,26.3416 7.2864,26.4296 12.2528,26.3416"
                                     id="Ellipse_1_copy_4-2"
                                     stroke={colors.blue2}
-                                    stroke-width="2.4"
+                                    strokeWidth="2.4"
                                     // style=""
                                 />
                                 <path
@@ -83,7 +79,7 @@ export const Robot: React.FC<IconProps> = (props) => (
                                     id="Mask"
                                     transform="matrix(1, 0, 0, 1, 0, 0)"
                                     stroke={colors.blue2}
-                                    stroke-width="2.4"
+                                    strokeWidth="2.4"
                                     // style=""
                                 />
                             </g>
@@ -91,7 +87,7 @@ export const Robot: React.FC<IconProps> = (props) => (
                                 d="M82.6872,60.4064 C82.6872,60.4064 47.4496,60.428 47.4496,101.7872 L47.4496,110 C47.4496,139.6216 68.236,148.6536 74.4,150.4312 C81.0448,152.3512 86.4648,155.2008 111.9664,155.2008 C125.4928,155.2008 176.7208,156.8712 176.7208,110.0008 L176.7208,101.7872 C176.7208,60.4272 141.4824,60.4064 141.4824,60.4064 L82.6872,60.4064 Z"
                                 id="Shape_1_copy_2"
                                 stroke={colors.blue2}
-                                stroke-width="2.4"
+                                strokeWidth="2.4"
                                 // style=""
                             />
                             <g
@@ -103,7 +99,7 @@ export const Robot: React.FC<IconProps> = (props) => (
                                         d="M0.112803789,0.4 L49.8192004,0.4 C49.8192004,0.4 49.9009782,16.7104 24.8020759,16.7104 C0.993581304,16.7104 0.112803789,0.4 0.112803789,0.4 Z"
                                         transform="matrix(1, 0, 0, 1, 0, 0)"
                                         fill={colors.blue2}
-                                        fill-rule="evenodd"
+                                        fillRule="evenodd"
                                     />
                                     <path
                                         d="M0.112803789,0.4 L49.8192004,0.4 C49.8192004,0.4 49.9009782,16.7104 24.8020759,16.7104 C0.993581304,16.7104 0.112803789,0.4 0.112803789,0.4 Z"
@@ -120,15 +116,15 @@ export const Robot: React.FC<IconProps> = (props) => (
                             <g
                                 id="Group"
                                 transform="translate(72.000000, 82.400000)"
-                                fill-rule="nonzero"
+                                fillRule="nonzero"
                             >
                                 <g id="Group-4">
-                                    <g id="Ellipse_1_copy_5" stroke-width="2.4">
+                                    <g id="Ellipse_1_copy_5" strokeWidth="2.4">
                                         <circle
                                             stroke={colors.blue2}
-                                            stroke-linejoin="square"
+                                            //  strokeLinejoin="square"
                                             fill={colors.blue2}
-                                            fill-rule="evenodd"
+                                            fillRule="evenodd"
                                             cx="11.1752"
                                             cy="11.4872"
                                             r="9.5752"
@@ -146,15 +142,15 @@ export const Robot: React.FC<IconProps> = (props) => (
                             <g
                                 id="Group-Copy"
                                 transform="translate(125.600000, 82.400000)"
-                                fill-rule="nonzero"
+                                fillRule="nonzero"
                             >
                                 <g id="Group-4">
-                                    <g id="Ellipse_1_copy_5" stroke-width="2.4">
+                                    <g id="Ellipse_1_copy_5" strokeWidth="2.4">
                                         <circle
                                             stroke={colors.blue2}
-                                            stroke-linejoin="square"
+                                            //   strokeLinejoin="square"
                                             fill={colors.blue2}
-                                            fill-rule="evenodd"
+                                            fillRule="evenodd"
                                             cx="11.1752"
                                             cy="11.4872"
                                             r="9.5752"
