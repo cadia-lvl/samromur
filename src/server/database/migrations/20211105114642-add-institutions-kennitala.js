a0435674efa962dcbe35f7aaba4457b399aac68c
staffan <staffanh@ru.is>
2021-11-05 12:58:33 +0000

    update db with confirmation logic competition

---
+++ src/server/database/migrations/20211105114642-add-institutions-kennitala.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
+};
+
+exports.up = async function (db) {
+    return db.runSql(`
+    ALTER TABLE
+        institutions
+    ADD COLUMN kennitala VARCHAR(10) DEFAULT NULL
+  `);
+};
+
+exports.down = function () {
+    return null;
+};
+
+exports._meta = {
+    version: 1,
+};
