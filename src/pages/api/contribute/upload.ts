d0f8a65b79de45be27e7918cc733a04cbb86a9aa
Þorsteinn Daði Gunnarsson <thdg90@gmail.com>
2024-06-18 13:18:22 +0000

    Upload icelandic proficiency

--- src/pages/api/contribute/upload.ts
+++ src/pages/api/contribute/upload.ts
@@ -46,6 +46,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const nativeLanguage = decodeURIComponent(
             headers.native_language as string
         );
+        const icelandicProficiency = decodeURIComponent(headers.icelandic_proficiency as string)
         const sampleRate = parseInt(headers.sample_rate as string) || undefined;
         const duration = parseFloat(headers.duration as string) || undefined;
         const size = parseFloat(headers.size as string) || undefined;
@@ -58,6 +59,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             gender,
             institution,
             nativeLanguage,
+            icelandicProficiency,
             sentence,
             userAgent,
             sampleRate: sampleRate && sampleRate,
